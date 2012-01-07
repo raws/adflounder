aad7499a12e8adef30d01550fc1cd84f066351e8
Ross Paffett <ross@rosspaffett.com>
2012-01-07 04:52:16 -0500

    Display link to target in overlay

--- Scripts/FindTarget.js
+++ Scripts/FindTarget.js
@@ -7,7 +7,14 @@ for (var i = 0; i < scripts.length; i++) {
 			var match = html.match(/var url = '(http.*?)'/i)
 			if (match !== null && match !== undefined && match.length >= 2) {
 				var target = match[1];
-				// TODO Do something
+				
+				document.getElementById("Interstitual").style.zIndex = "0";
+				
+				var overlay = document.createElement("div");
+				overlay.id = "adflounder-overlay";
+				overlay.innerHTML = "<a id='adflounder-link' href='" + target + "'>" + target + "</a>";
+				document.body.lastChild.insertAdjacentElement("afterEnd", overlay);
+				
 				break;
 			}
 		}
